78261ca9571a5efaa1774d16d243035fd543d7b3
Martin Dev <u2001uk@yahoo.co.uk>
2021-02-22 23:22:39 +0000

    feature/added javascript email validation
    feature/added javascript email validation

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let email = document.getElementById("emailaddress");
-console.log(emailaddress)
+function validateForm(x) {
+    var x = document.forms["myform"]["emailaddress"].value;
+    let mailformat = "/^\w+([\.-]?\w+)+@\w+([\.:]?\w+)+(\.[a-zA-Z0-9]{2,3})+$/";
+
+    if (x = "") {
+        alert("Oops! Please add your email")
+        return
+    }
+
+    else if (x.match(mailformat)) {
+        alert("HAPPY FACE");
+        return
+    }
+
+    else {
+        alert("Oops! Please check your email");
+        return
+    }
+}
